63da6f62ad6077b77a582da8ed6a43353059ddaa
gab <gabriel.paquette@polymtl.ca>
2020-02-05 20:10:29 -0500

    GP: cours 1

---
+++ New Unity Project/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float horizontalAxis;
+    float verticalAxis;
+    [SerializeField] float speed = 5;
+
+    void Update()
+    {
+        GetInputs();
+
+        transform.position += transform.forward * verticalAxis * speed * Time.deltaTime; 
+        transform.position += transform.right * horizontalAxis * speed * Time.deltaTime; 
+    }
+
+    private void GetInputs()
+    {
+        horizontalAxis = Input.GetAxis("Horizontal");
+        verticalAxis = Input.GetAxis("Vertical");
+    }   
+}
